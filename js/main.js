c80241cea211636d3a7b9ab7484e8984177fae44
Rahul Rajabhoj <rrajabhoj@ml.local>
2021-07-13 14:50:32 +0530

    Added firstName field and form submit bindings

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 function CreateAccountViewModel() {
     var self = this;
+
+    self.firstName = ko.observable();
+
+    self.firstName.subscribe(function(newValue) {
+        console.log(newValue);
+    })
+
+    self.handleSubmit = function() {
+        console.log('SUBMIT FORM');
+
+        var payload = {
+            firstName: self.firstName()
+        }
+
+        console.log(payload);
+    }
 }
 
 ko.applyBindings(new CreateAccountViewModel(), document.querySelector("#knockout-app"));
